7f3768b3d4f4c5a307cf1afda5dce95df2fd3fd2
beanto03 <64835144+beanto03@users.noreply.github.com>
2024-09-30 21:10:14 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Create a library instance
+
+        //Declare object
         Library library = new Library();
+        User user = new User(null, 0);
+        Book book = new Book(null, null, null);
+        Scanner sc = new Scanner(System.in);
+        
 
         // Create some books
         Book book1 = new Book("Sofea Jane", "Black Hanifah", "12345");
@@ -27,23 +33,56 @@ public class Main {
 
         System.out.println("Welcome to Laman Norsham Library");
         System.out.println("Please choose desired menu :");
-        System.out.println("1. View all available books");
+        System.out.println("1. View all available books"); //library.listAvailableBooks();
         System.out.println("2. Borrow book"); //user.borrowBook()
         System.out.println("3. Print borrowed books"); //printBorrowBooks()
         System.out.println("4. Print book details"); //printBookInfo()
         System.out.println("5. Add book to library");
-        System.out.println("6. Search book title"); //library.searchByTitle
-        System.out.println("7. View book due date"); //user.checkDueDates()
+        System.out.println("6. View book due date"); //library.searchByTitle //user.checkDueDates()
+        System.out.println("7. Return book by title ");
+        int userInput = sc.nextInt();
+        sc.nextLine(); // flush data entry
 
-
-        // List available books
-        library.listAvailableBooks();
-
-        // Print borrowed books
-        library.getBorrowedBooksForAllUsers();
-
-        // Check due dates and overdue books
-        user1.checkDueDates();
-        user1.checkOverdueBooks();
+        switch (userInput){
+        case 1:// Done
+            library.listAvailableBooks();
+            break;
+        case 2: // done
+            System.out.println("Enter user name: ");
+            String borrowName = sc.nextLine();
+            user.setName(borrowName);
+            System.out.println("Enter book title: ");
+            String bookName = sc.nextLine();
+            library.borrowBook(user, bookName); // this line
+            break;
+        case 3: // done
+            library.getBorrowedBooksForAllUsers();
+            break;
+        case 4: //Done
+            System.out.println("Enter book title: ");
+            String bookInput = sc.nextLine();
+            library.searchByTitle(bookInput);
+            break;
+        case 5: // done
+            System.out.println("Enter book title: ");
+            String titleInput = sc.nextLine();
+            System.out.println("Enter author name: ");
+            String nameInput = sc.nextLine();
+            System.out.println("Enter ISBN number: ");
+            String isbnInput = sc.nextLine();
+            library.addBook(new Book(titleInput, nameInput, isbnInput));
+            break;
+        case 6: // still need to repair - View book due date
+            
+            break;
+        case 7: //not ok
+            System.out.println("Enter book title");
+            String returnInput = sc.nextLine();
+            user.returnBook(returnInput);
+            break;
+        default:
+            System.out.println("You enter wrong input");
+            break;
+        }
     }
 }
